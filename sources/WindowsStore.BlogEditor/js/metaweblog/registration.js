ec59612092b98c6fb8e85116723b883c8b619e45
Denis Gladkikh <outcoldman@outlook.com>
2012-11-27 22:54:09 -0800

    Use JS methods for XML

--- sources/WindowsStore.BlogEditor/js/metaweblog/registration.js
+++ sources/WindowsStore.BlogEditor/js/metaweblog/registration.js
@@ -5,25 +5,38 @@
 (function () {
     "use strict";
 
-    var xmlDocument = Windows.Data.Xml.Dom.XmlDocument;
+    // use this document for creating XML
+    var doc = document.implementation.createDocument(null, null, null);
 
-    function parseUserBlogs(body) {
-        var document = new xmlDocument();
-        document.loadXml(body);
-        // get methodResponse
-        var nodesBlogInfos = document.selectNodes('methodResponse/params/param/value/array/data/value/struct');
+    // function that creates the XML structure
+    function x() {
+        var node = doc.createElement(arguments[0]), text, child;
 
+        for (var i = 1; i < arguments.length; i++) {
+            child = arguments[i];
+            if (typeof child == 'string') {
+                child = doc.createTextNode(child);
+            }
+            node.appendChild(child);
+        }
+
+        return node;
+    };
+
+    function parseUserBlogs(xml) {
         var blogs = new Array();
+        
+        var items = xml.querySelectorAll("methodResponse > params > param > value > array > data > value > struct");
 
-        for (var i = 0; i < nodesBlogInfos.length; i++) {
-            var nodeStruct = nodesBlogInfos[i];
-            var nodesBlogInfoProperties = nodeStruct.selectNodes('member');
+        for (var i = 0; i < items.length; i++) {
+            var nodeStruct = items[i];
+            var nodesBlogInfoProperties = nodeStruct.querySelectorAll('member');
             var blogInfoProperties = {};
             for (var j = 0; j < nodesBlogInfoProperties.length; j++) {
                 var nodeProperty = nodesBlogInfoProperties[j];
-                var nodeName = nodeProperty.selectSingleNode('name');
-                var nodeValue = nodeProperty.selectSingleNode('value');
-                blogInfoProperties[nodeName.innerText.toLowerCase()] = nodeValue.innerText;
+                var nodeName = nodeProperty.querySelector('name');
+                var nodeValue = nodeProperty.querySelector('value');
+                blogInfoProperties[nodeName.textContent.toLowerCase()] = nodeValue.textContent;
             }
             blogs.push(new OutcoldSolutions.BlogEditor.MetaWeblog.BlogInfo(blogInfoProperties['blogid'], blogInfoProperties['url'], blogInfoProperties['blogname']));
         }
@@ -36,27 +49,30 @@
             this._blogSettings = blogSettings;
         }, {
             load: function () {
-                var document = new xmlDocument();
-                var methodCall = document.createElement('methodCall');
-                document.appendChild(methodCall);
 
-                var methodName = document.createElement('methodName');
-                methodName.innerText = 'blogger.getUsersBlogs';
-                methodCall.appendChild(methodName);
-
-                var params = document.createElement('params');
-                var paramValues = ['', this._blogSettings.userName, this._blogSettings.password];
-                for (var i = 0; i < paramValues.length; i++) {
-                    var nodeParam = document.createElement('param');
-                    var nodeValue = document.createElement('value');
-                    nodeValue.innerText = paramValues[i];
-                    nodeParam.appendChild(nodeValue);
-                    params.appendChild(nodeParam);
-                }
-
-                methodCall.appendChild(params);
-
-                var requestBody = document.getXml();
+                var xml = x('methodCall',
+                    x('methodName', 'blogger.getUsersBlogs'),
+                    x('params',
+                        x('param',
+                            x('value',
+                                x('string', '')
+                            )
+                        ),
+                        x('param',
+                            x('value',
+                                x('string', this._blogSettings.userName)
+                            )
+                        ),
+                        x('param',
+                            x('value',
+                                x('string', this._blogSettings.password)
+                            )
+                        )
+                    )
+                );
+                
+               
+                var requestBody = new XMLSerializer().serializeToString(xml).toString();
 
                 return WinJS.xhr({
                     type: "post",
@@ -64,7 +80,7 @@
                     headers: { "Content-Type": "text/xml; charset=UTF-8", "Content-Length": requestBody.length },
                     data: requestBody
                 }).then(function (result) {
-                    return parseUserBlogs(result.responseText);
+                    return parseUserBlogs(result.responseXML);
                 });
             }
         })
